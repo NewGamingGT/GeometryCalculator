97be4f46812cdde150a707ec84ec65a345666aeb
NewGaming <39025657+NewGamingGT@users.noreply.github.com>
2018-12-21 01:28:57 +0800

    Version 0.01

---
+++ FormularCalculator/FormularCalculator/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace FormularCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LengthLbl = new System.Windows.Forms.Label();
+            this.lengthTbx = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.formulaLbl = new System.Windows.Forms.Label();
+            this.squareBtn = new System.Windows.Forms.Button();
+            this.rectBtn = new System.Windows.Forms.Button();
+            this.trapBtn = new System.Windows.Forms.Button();
+            this.triBtn = new System.Windows.Forms.Button();
+            this.answerLbl = new System.Windows.Forms.Label();
+            this.breadthTbx = new System.Windows.Forms.TextBox();
+            this.breadthLbl = new System.Windows.Forms.Label();
+            this.baseTbx = new System.Windows.Forms.TextBox();
+            this.baseLbl = new System.Windows.Forms.Label();
+            this.circleBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LengthLbl
+            // 
+            this.LengthLbl.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.LengthLbl.Font = new System.Drawing.Font("Arial", 20F);
+            this.LengthLbl.ForeColor = System.Drawing.Color.White;
+            this.LengthLbl.Location = new System.Drawing.Point(37, 399);
+            this.LengthLbl.Name = "LengthLbl";
+            this.LengthLbl.Size = new System.Drawing.Size(113, 38);
+            this.LengthLbl.TabIndex = 0;
+            this.LengthLbl.Text = "Length:";
+            this.LengthLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lengthTbx
+            // 
+            this.lengthTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lengthTbx.ForeColor = System.Drawing.Color.White;
+            this.lengthTbx.Location = new System.Drawing.Point(156, 399);
+            this.lengthTbx.Name = "lengthTbx";
+            this.lengthTbx.Size = new System.Drawing.Size(93, 38);
+            this.lengthTbx.TabIndex = 1;
+            this.lengthTbx.Text = "1";
+            this.lengthTbx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lengthTbx_KeyPress);
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.calculateBtn.ForeColor = System.Drawing.Color.White;
+            this.calculateBtn.Location = new System.Drawing.Point(675, 403);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(113, 35);
+            this.calculateBtn.TabIndex = 2;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // formulaLbl
+            // 
+            this.formulaLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formulaLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.formulaLbl.ForeColor = System.Drawing.Color.White;
+            this.formulaLbl.Location = new System.Drawing.Point(43, 31);
+            this.formulaLbl.Name = "formulaLbl";
+            this.formulaLbl.Size = new System.Drawing.Size(749, 39);
+            this.formulaLbl.TabIndex = 4;
+            this.formulaLbl.Text = "Formular for area of square = Length * Length";
+            this.formulaLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // squareBtn
+            // 
+            this.squareBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.squareBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.squareBtn.ForeColor = System.Drawing.Color.White;
+            this.squareBtn.Location = new System.Drawing.Point(12, 35);
+            this.squareBtn.Name = "squareBtn";
+            this.squareBtn.Size = new System.Drawing.Size(113, 35);
+            this.squareBtn.TabIndex = 5;
+            this.squareBtn.Text = "Square";
+            this.squareBtn.UseVisualStyleBackColor = true;
+            this.squareBtn.Click += new System.EventHandler(this.squareBtn_Click);
+            // 
+            // rectBtn
+            // 
+            this.rectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.rectBtn.ForeColor = System.Drawing.Color.White;
+            this.rectBtn.Location = new System.Drawing.Point(12, 76);
+            this.rectBtn.Name = "rectBtn";
+            this.rectBtn.Size = new System.Drawing.Size(113, 35);
+            this.rectBtn.TabIndex = 6;
+            this.rectBtn.Text = "Rectangle";
+            this.rectBtn.UseVisualStyleBackColor = true;
+            this.rectBtn.Click += new System.EventHandler(this.rectBtn_Click);
+            // 
+            // trapBtn
+            // 
+            this.trapBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.trapBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.trapBtn.ForeColor = System.Drawing.Color.White;
+            this.trapBtn.Location = new System.Drawing.Point(12, 158);
+            this.trapBtn.Name = "trapBtn";
+            this.trapBtn.Size = new System.Drawing.Size(113, 35);
+            this.trapBtn.TabIndex = 8;
+            this.trapBtn.Text = "Trapezium";
+            this.trapBtn.UseVisualStyleBackColor = true;
+            this.trapBtn.Click += new System.EventHandler(this.trapBtn_Click);
+            // 
+            // triBtn
+            // 
+            this.triBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.triBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.triBtn.ForeColor = System.Drawing.Color.White;
+            this.triBtn.Location = new System.Drawing.Point(12, 117);
+            this.triBtn.Name = "triBtn";
+            this.triBtn.Size = new System.Drawing.Size(113, 35);
+            this.triBtn.TabIndex = 7;
+            this.triBtn.Text = "Triangle";
+            this.triBtn.UseVisualStyleBackColor = true;
+            this.triBtn.Click += new System.EventHandler(this.triBtn_Click);
+            // 
+            // answerLbl
+            // 
+            this.answerLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.answerLbl.BackColor = System.Drawing.Color.Transparent;
+            this.answerLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.answerLbl.ForeColor = System.Drawing.Color.Black;
+            this.answerLbl.Location = new System.Drawing.Point(17, 196);
+            this.answerLbl.Name = "answerLbl";
+            this.answerLbl.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.answerLbl.Size = new System.Drawing.Size(775, 40);
+            this.answerLbl.TabIndex = 3;
+            this.answerLbl.Text = "1";
+            this.answerLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // breadthTbx
+            // 
+            this.breadthTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.breadthTbx.ForeColor = System.Drawing.Color.White;
+            this.breadthTbx.Location = new System.Drawing.Point(589, 399);
+            this.breadthTbx.Name = "breadthTbx";
+            this.breadthTbx.Size = new System.Drawing.Size(80, 38);
+            this.breadthTbx.TabIndex = 10;
+            this.breadthTbx.Text = "1";
+            this.breadthTbx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.breadthTbx_KeyPress);
+            // 
+            // breadthLbl
+            // 
+            this.breadthLbl.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.breadthLbl.Font = new System.Drawing.Font("Arial", 20F);
+            this.breadthLbl.ForeColor = System.Drawing.Color.White;
+            this.breadthLbl.Location = new System.Drawing.Point(465, 399);
+            this.breadthLbl.Name = "breadthLbl";
+            this.breadthLbl.Size = new System.Drawing.Size(118, 38);
+            this.breadthLbl.TabIndex = 9;
+            this.breadthLbl.Text = "Breath:";
+            this.breadthLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // baseTbx
+            // 
+            this.baseTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.baseTbx.ForeColor = System.Drawing.Color.White;
+            this.baseTbx.Location = new System.Drawing.Point(379, 399);
+            this.baseTbx.Name = "baseTbx";
+            this.baseTbx.Size = new System.Drawing.Size(80, 38);
+            this.baseTbx.TabIndex = 12;
+            this.baseTbx.Visible = false;
+            this.baseTbx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.baseTbx_KeyPress);
+            // 
+            // baseLbl
+            // 
+            this.baseLbl.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.baseLbl.Font = new System.Drawing.Font("Arial", 20F);
+            this.baseLbl.ForeColor = System.Drawing.Color.White;
+            this.baseLbl.Location = new System.Drawing.Point(255, 399);
+            this.baseLbl.Name = "baseLbl";
+            this.baseLbl.Size = new System.Drawing.Size(118, 38);
+            this.baseLbl.TabIndex = 11;
+            this.baseLbl.Text = "Base 2:";
+            this.baseLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.baseLbl.Visible = false;
+            // 
+            // circleBtn
+            // 
+            this.circleBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.circleBtn.ForeColor = System.Drawing.Color.White;
+            this.circleBtn.Location = new System.Drawing.Point(12, 199);
+            this.circleBtn.Name = "circleBtn";
+            this.circleBtn.Size = new System.Drawing.Size(113, 35);
+            this.circleBtn.TabIndex = 13;
+            this.circleBtn.Text = "Circle";
+            this.circleBtn.UseVisualStyleBackColor = true;
+            this.circleBtn.Click += new System.EventHandler(this.circleBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.circleBtn);
+            this.Controls.Add(this.baseTbx);
+            this.Controls.Add(this.baseLbl);
+            this.Controls.Add(this.breadthTbx);
+            this.Controls.Add(this.breadthLbl);
+            this.Controls.Add(this.trapBtn);
+            this.Controls.Add(this.triBtn);
+            this.Controls.Add(this.rectBtn);
+            this.Controls.Add(this.squareBtn);
+            this.Controls.Add(this.formulaLbl);
+            this.Controls.Add(this.answerLbl);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.lengthTbx);
+            this.Controls.Add(this.LengthLbl);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(820, 500);
+            this.MinimumSize = new System.Drawing.Size(820, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geometry Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LengthLbl;
+        private System.Windows.Forms.TextBox lengthTbx;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label formulaLbl;
+        private System.Windows.Forms.Button squareBtn;
+        private System.Windows.Forms.Button rectBtn;
+        private System.Windows.Forms.Button trapBtn;
+        private System.Windows.Forms.Button triBtn;
+        private System.Windows.Forms.Label answerLbl;
+        private System.Windows.Forms.TextBox breadthTbx;
+        private System.Windows.Forms.Label breadthLbl;
+        private System.Windows.Forms.TextBox baseTbx;
+        private System.Windows.Forms.Label baseLbl;
+        private System.Windows.Forms.Button circleBtn;
+    }
+}
+
---
+++ FormularCalculator/FormularCalculator/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularCalculator
+{
+    public partial class Form1 : Form
+    {
+        int rectWidth;
+        int rectHeight;
+
+        int formHeight;
+        int formWidth;
+
+        int xPos;
+        int yPos;
+
+        int mode = 0;
+
+        string Length;
+        string Breadth;
+        string _base;
+
+        decimal lengthNum;
+        decimal breadthNum;
+        decimal baseNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+            formHeight = this.Height;
+            formWidth = this.Width;
+            rectWidth = 250;
+            rectHeight = 250;
+
+            breadthLbl.Visible = false;
+            breadthTbx.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        //Formulas
+        #region Formulas
+        decimal squareArea(decimal length)
+        {
+            return length * length;
+        }
+
+        decimal rectArea(decimal length, decimal breadth)
+        {
+            return length * breadth;
+        }
+
+        decimal triArea(decimal length, decimal height)
+        {
+            return (length * height) / 2;
+        }
+
+        decimal trapArea(decimal base1,decimal base2,decimal height)
+        {
+            return ((base1 + base2) / 2) * height;
+        }
+
+        decimal circleArea(decimal radius)
+        {
+            decimal pi = 3.14159m;
+            return pi * radius * radius;
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal realAnswer = 0;
+
+            Length = lengthTbx.Text;
+            Breadth = breadthTbx.Text;
+            _base = baseTbx.Text;
+
+            if (lengthTbx.Text != string.Empty)
+            {
+                lengthNum = Convert.ToDecimal(Length);
+                if(breadthTbx.Text != string.Empty)
+                {
+                    breadthNum = Convert.ToDecimal(Breadth);
+                    if (baseTbx.Text != string.Empty)
+                    {
+                        baseNum = Convert.ToDecimal(_base);
+                    }
+                }
+            }
+            
+            if (mode == 0)
+            {
+                realAnswer = squareArea(lengthNum);
+            }
+            else if(mode == 1)
+            {
+                realAnswer = rectArea(lengthNum, breadthNum);
+            }
+            else if(mode == 2)
+            {
+                realAnswer = triArea(lengthNum, breadthNum);
+            }
+            else if(mode == 3)
+            {
+                realAnswer = trapArea(lengthNum, baseNum, breadthNum);
+            }
+            else if(mode == 4)
+            {
+                realAnswer = circleArea(lengthNum);
+            }
+            answerLbl.Text = realAnswer.ToString();
+        }
+
+        SolidBrush myBrush = new SolidBrush(Color.White);
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            xPos = (formWidth / 2) - (rectWidth / 2) - 10;
+            yPos = (formHeight / 2) - (rectHeight / 2) - 25;
+
+            if (mode == 0 || mode == 1)
+            {
+                e.Graphics.FillRectangle(myBrush, xPos, yPos, rectWidth, rectHeight);
+            }
+            else if (mode == 2)
+            {
+                Point[] a = { new Point(400, 100), new Point(525, 350), new Point(275, 350) };
+                e.Graphics.FillPolygon(myBrush, a);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+            else if (mode == 3)
+            {
+                Point[] a = { new Point(300, 100), new Point(500, 100), new Point(600, 350), new Point(200, 350) };
+                e.Graphics.FillPolygon(myBrush, a);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+            else if (mode == 4)
+            {
+                e.Graphics.FillEllipse(myBrush, xPos, yPos, rectWidth, rectHeight);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+        }
+
+        private void squareBtn_Click(object sender, EventArgs e)
+        {
+            Visibility(false, false, false, false);
+
+            mode = 0;
+
+            breadthLbl.Text = "Breadth:";
+            LengthLbl.Text = "Length:";
+
+            Reset();        
+
+            formulaLbl.Text = "Formular for area of square =  Length * Length";
+
+            rectWidth = 250;
+            rectHeight = 250;
+            Invalidate();
+        }
+
+        private void rectBtn_Click(object sender, EventArgs e)
+        {
+            Visibility(true, true, false, false);
+
+            breadthLbl.Text = "Breadth:";
+            LengthLbl.Text = "Length:";
+
+            mode = 1;
+
+            formulaLbl.Text = "Formular for area of rectangle =  Length * Breadth";
+
+            Reset();
+
+            rectWidth = 400;
+            rectHeight = 250;
+            Invalidate();          
+        }
+
+        private void triBtn_Click(object sender, EventArgs e)
+        {
+            Visibility(true, true, false, false);
+
+            breadthLbl.Text = "Height:";
+            LengthLbl.Text = "Length:";
+
+            mode = 2;
+
+            formulaLbl.Text = "Formular for area of triangle =  (Length * Height) / 2";
+
+            Reset();
+
+            Invalidate();
+        }
+
+        private void trapBtn_Click(object sender, EventArgs e)
+        {
+            Visibility(true, true, true, true);
+
+            breadthLbl.Text = "Height:";
+            LengthLbl.Text = "Base 1:";
+            baseLbl.Text = "Base 2:";
+
+            mode = 3;
+
+            formulaLbl.Text = "Formular for area of trapezium =  ((Base 1 + Base 2) / 2) * Height";
+
+            Reset();
+
+            Invalidate();
+        }
+
+        private void circleBtn_Click(object sender, EventArgs e)
+        {
+            Visibility(false, false, false, false);
+
+            LengthLbl.Text = "Radius:";
+
+            mode = 4;
+
+            formulaLbl.Text = "Formular for area of circle =  pi * radius * radius";
+
+            Reset();
+
+            rectWidth = 250;
+            rectHeight = 250;
+
+            Invalidate();
+        }   
+
+        private void Reset()
+        {
+            answerLbl.Text = string.Empty;
+            lengthTbx.Text = string.Empty;
+            breadthTbx.Text = string.Empty;
+            baseTbx.Text = string.Empty;
+        }
+
+        private void Visibility(bool breaLbl, bool breaTbx, bool basLbl, bool basTbx)
+        {
+            breadthLbl.Visible = breaLbl;
+            breadthTbx.Visible = breaTbx;
+            baseLbl.Visible = basLbl;
+            baseTbx.Visible = basTbx;
+        }
+
+
+        //Only allow numbers and . in textbox
+        #region Textbox
+        private void lengthTbx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnlyNumber(sender, e);
+        }
+
+        private void breadthTbx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnlyNumber(sender, e);
+        }
+
+        private void baseTbx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnlyNumber(sender, e);
+        }
+
+        private static void OnlyNumber(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                        && !char.IsDigit(e.KeyChar)
+                        && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+        #endregion
+    }
+}
